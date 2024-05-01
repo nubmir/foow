7651c9ae05e215d58291955d77090a8dda151e79
IbrhmSptra <sptraibrhm@gmail.com>
2024-05-01 12:01:11 +0700

    init vercel, fix : route index

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome to the Foodo API");
 });
 app.use("/api", apiRoutes);
